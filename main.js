2ab678fe6963b9ba4d02ba5127dbf914afddf276
Anderson Mohr <anderson.mohr@digitaldesk.com.br>
2023-12-15 17:14:50 -0300

    Adicionado alerta com javascript

---
+++ main.js
@@ -0,0 +1,11 @@
+const enviar = document.querySelector("#btnenviar")
+
+enviar.addEventListener("click", () =>{
+    alert("Mensagem enviada! O envio é fake kk")
+});
+
+const projeto = document.querySelector("#botao")
+
+projeto.addEventListener("Click", () =>{
+    alert("Mensagem do projeto!")
+});
