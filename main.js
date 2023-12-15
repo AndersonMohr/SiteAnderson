f9240aa3b87df2748d458b67bb6ff16b34a0656f
Anderson Mohr <anderson.mohr@digitaldesk.com.br>
2023-12-15 17:43:19 -0300

    Arrumando o botão da aba projetos e contato.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const enviar = document.querySelector("#btnenviar")
 
-enviar.addEventListener("click", () =>{
-    alert("Mensagem enviada! O envio é fake kk")
-});
+if(enviar){
+    enviar.addEventListener("click", () =>{
+        alert("Mensagem enviada! O envio é fake kk")
+    });
+}
 
 const projeto = document.querySelector("#botao")
-
-projeto.addEventListener("Click", () =>{
-    alert("Mensagem do projeto!")
-});
+if(projeto){
+    projeto.addEventListener("click", () =>{
+        alert("Mensagem do projeto!")
+    });
+}
